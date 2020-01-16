5536d8ab90b3d3740ef194524acd18a0179c4ff8
xiaohuli <waterdoor@foxmail>
2020-01-16 10:54:05 +0800

    fix: 包内容

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+import netModel from './network';
+
+const writeCookie = (targetObj = {}, expireDays = 20) => {
+    const targetExpireTime = new Date();
+    targetExpireTime.setTime(targetExpireTime.getTime() + expireDays * 24 * 60 * 60 * 1000);
+
+    for (let key in targetObj) {
+        document.cookie = key + '=' + escape(targetObj[key]) + ';expires=' + targetExpireTime.toGMTString();
+    }
+}
+
+const parseCookie = () => {
+    const result = {};
+    if(typeof document === 'undefined') {
+        return {}
+    }
+    document.cookie.split('; ').map(item => {
+        const temp = item.split('=');
+        result[temp[0]] = temp[1];
+    })
+    return result;
+}
+
+const parseCookieObjToString = (obj) => {
+    let ans = '';
+    for (let key in obj) {
+        ans += key + '=' + escape(obj[key]) + ';';
+    }
+    return ans;
+}
+export {parseCookieObjToString};
+export {writeCookie};
+export {parseCookie};
+export {netModel};
+
 export function getYearMonthDate() {
     const now = new Date();
     //  getMonth是按照数组来保存的，会比实际月份少1
@@ -5,5 +40,9 @@ export function getYearMonthDate() {
 }
 
 export default {
-    getYearMonthDate
+    getYearMonthDate,
+    parseCookieObjToString,
+    writeCookie,
+    parseCookie,
+    netModel
 }
